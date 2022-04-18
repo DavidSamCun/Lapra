21ea8807a6b49e40ccc6c2047eaae1526119f813
David <davidsamcun@gmail.com>
2022-04-17 23:41:44 -0400

    test

---
+++ Lapra/src/main/java/com/mycompany/myapp/Model/Laptop.java
@@ -0,0 +1,79 @@
+package com.mycompany.myapp.Model;
+
+public class Laptop {
+
+    private String make;
+    private String model;
+    private float size;
+    private int memory;
+    private int storage;
+    private int screenBrightness;
+    private int refreshRateHz;
+    private float price;
+
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    public int getScreenBrightness() {
+        return screenBrightness;
+    }
+
+    public void setScreenBrightness(int screenBrightness) {
+        this.screenBrightness = screenBrightness;
+    }
+
+    public int getRefreshRateHz() {
+        return refreshRateHz;
+    }
+
+    public void setRefreshRateHz(int refreshRateHz) {
+        this.refreshRateHz = refreshRateHz;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
